5cb324a5d1fc7df7f1b51940a52248726f895ee8
udenis80 <udenis80@mail.ru>
2023-04-20 21:43:42 +0300

    exchange template

--- choco/settings.py
+++ choco/settings.py
@@ -125,5 +125,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+TEMPLATE_DIRS = [
+    os.path.join(BASE_DIR, 'templates/products'),
 
+]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- chocolate/views.py
+++ chocolate/views.py
@@ -4,19 +4,9 @@ from django.views import View
 from .models import Product
 
 def index(request):
-    return render(request, 'products/index.html')
+    return render(request, 'index.html')
 
 
-def shop(request):
-    products = Product.objects.all()
-    return render(request, 'products/shop.html', {'pr': products})
-
-
-def cart(request):
-    return render(request, 'products/cart.html')
-
-def checkout(request):
-    return render(request, 'products/checkout.html')
-
-def product_detail(request):
-    return render(request, 'products/product-details.html')
+# def shop(request):
+#     products = Product.objects.all()
+#     return render(request, 'products/shop.html', {'pr': products})
